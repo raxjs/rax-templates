bc931716f8ae6f46e02dac883a141c5407a1f567
yacheng.sz <yacheng.sz@taobao.com>
2020-02-18 10:36:52 +0800

    feat: remove unuse module

--- template-modules-render/packages/universal-data-processor/src/getModuleData.js
+++ template-modules-render/packages/universal-data-processor/src/getModuleData.js
@@ -1,22 +1,22 @@
 let getItem = (item) => {
-    let {
-      data,
-      data_id,
-      childIndex,
-    } = item;
-  
-    let rowId = `${data_id}-${childIndex}`;
-    const isFirstCell = `${data_id}-0` === rowId;
-    let itemData = {
-      ...item,
-      id: rowId,
-      rowIndex: childIndex,
-      cellLength: data.length, // TODO
-      isFirstCell,
-      isLastCell: (rowId === rowId + '-' + (data.length - 1)),
-    };
-    
-    return itemData
+  let {
+    data,
+    data_id,
+    childIndex,
+  } = item;
+
+  let rowId = `${data_id}-${childIndex}`;
+  const isFirstCell = `${data_id}-0` === rowId;
+  let itemData = {
+    ...item,
+    id: rowId,
+    rowIndex: childIndex,
+    cellLength: data.length,
+    isFirstCell,
+    isLastCell: (rowId === rowId + '-' + (data.length - 1)),
   };
   
-  export default getItem;
+  return itemData
+};
+
+export default getItem;
--- template-modules-render/src/mockData/modules.js
+++ template-modules-render/src/mockData/modules.js
@@ -1,10 +1,8 @@
-import Banner from '../modules/Banner';
 import CardA from '../modules/CardA';
 import CardB from '../modules/CardB';
 import CardC from '../modules/CardC';
 
 const data = {
-  'banner': Banner,
   'card_a': CardA,
   'card_b': CardB,
   'card_c': CardC,
